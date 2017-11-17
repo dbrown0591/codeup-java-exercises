f8eacc5dfe904399e997db6aa7eb9fa82a1f5fc9
damionbrown <dbrown0591>
2017-11-17 15:40:35 -0600

    first commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,52 @@
-package PACKAGE_NAME;
+
+//
+
 
 public class HelloWorld {
+    public static void main(String[] args) {
+//        int myFavoriteNumber = 3;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Damion";
+//        System.out.println(myString);
+
+//        //char cannot be converted to string
+//         myString = 'D';
+//        System.out.println(myString);
+
+//        //can't assign doubles to string variables
+//         myString = 3.14159;
+//        System.out.println(myString);
+//
+//            long myNumber =
+//            System.out.print(myNumber);
+//
+//             myNumber = 3.14;
+//            System.out.print(myNumber);
+//
+//            long myNumber = 123L;
+//            System.out.print(myNumber);
+//
+//            myNumber = 123;
+//            System.out.print(myNumber);
+////
+////            change float to double
+//            float myNumber = 3.14;
+//            System.out.print(myNumber);
+//
+//            int x = 5;
+//            System.out.println(x++);
+//            System.out.println(x);
+//
+//        class x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+    }
 }
+
+
+
+
+
+
+
