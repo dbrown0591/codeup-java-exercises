3764b2c7586644081ba0cdbd0eb46762c80d582e
damionbrown <dbrown0591>
2018-03-10 16:35:03 -0600

    Arithmetric

---
+++ src/Arithmetic.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+public class Practice {
+
+
+    //// 1).
+    public static int addition(int add1, int add2) {
+        return add1 + add2;
+    }
+
+    public static int subtraction(int sub1, int sub2) {
+        return sub1 - sub2;
+    }
+
+    public static int multiplication(int mult1, int mult2) {
+        return mult1 * mult2;
+    }
+
+    public static int division(int div1, int div2) {
+        return div1 / div2;
+    }
+
+    public static int modulus(int mod1, int mod2) {
+        return mod1 % mod2;
+    }
+
+
+
+    public static void main(String[] args) {
+
+
+        System.out.println("I want you to add two different numbers  ");
+
+        System.out.println("So let's begin ");
+
+        System.out.print("Please type a number ");
+        Scanner s = new Scanner(System.in);
+        int add1 = s.nextInt();
+
+        System.out.print("Type another number ");
+        Scanner t = new Scanner(System.in);
+        int add2 = t.nextInt();
+        System.out.println(addition(add1, add2));
+
+        System.out.println("Isn't this exciting!!  ");
+
+        System.out.print("Now I want you to subtract two different numbers ");
+
+        Scanner f = new Scanner(System.in);
+        int sub1 = f.nextInt();
+
+        System.out.print("Type another number ");
+        Scanner g = new Scanner(System.in);
+        int sub2 = g.nextInt();
+        System.out.println(subtraction(sub1, sub2));
+
+        System.out.println("Try multiplying two numbers!!  ");
+
+        System.out.print("Go on!  ");
+        Scanner m = new Scanner(System.in);
+        int mult1 = m.nextInt();
+
+        System.out.print("Again!  ");
+        Scanner n = new Scanner(System.in);
+        int mult2 = n.nextInt();
+        System.out.println(multiplication(mult1, mult2));
+
+        System.out.println("I can't believe it. I can't believe it!!  ");
+
+        System.out.print("One more! But division this time  ");
+        Scanner o = new Scanner(System.in);
+        int div1 = o.nextInt();
+
+        System.out.print("Again!  ");
+        Scanner p = new Scanner(System.in);
+        int div2 = p.nextInt();
+        System.out.println(division(div1, div2));
+
+        System.out.print("Ok ok! This is the last one ");
+        System.out.println("Modulus!");
+        Scanner q = new Scanner(System.in);
+        int mod1 = q.nextInt();
+
+        System.out.print("Again!  ");
+        Scanner r = new Scanner(System.in);
+        int mod2 = r.nextInt();
+        System.out.println(modulus(mod1, mod2));
+        System.out.println("Congratulations!!! Your all done!!");
+        return;
+
+
+    }
+
+
+
+
+        }
+
