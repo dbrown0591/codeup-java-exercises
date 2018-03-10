77c71f05c129cd5143734ece96c5abdd5f5dcd8e
Dan Brown <32341078+dbrown0591@users.noreply.github.com>
2018-03-10 16:26:03 -0600

    Update Range.java

--- src/Range.java
+++ src/Range.java
@@ -1,4 +1,26 @@
 package PACKAGE_NAME;
 
 public class Range {
+   public static void main(String[] args){
+
+        System.out.println(getInteger(1,10));
+    }
+    public static int getInteger(int min, int max) {
+        System.out.println("Please enter a number between " + min + " and " + max);
+        //I would like to return a string. Saying try again.
+        int userInput;
+
+        int no = 0;
+
+        Scanner input = new Scanner(System.in);
+        userInput = input.nextInt();
+
+        if (userInput <= min || userInput >= max) {
+            return no;
+        }
+        else{
+            return userInput;
+        }
+
+    }
 }
